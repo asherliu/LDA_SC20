edd3745beb46c4df1555bf4e0930f16b5a19382d
wojiushishen <wangshilong789@gmail.com>
2020-06-05 18:28:09 -0400

    Update Argument.cuh

--- EZLDA/Argument.cuh
+++ EZLDA/Argument.cuh
@@ -5,7 +5,7 @@ const int BlockDim = 256;
 const int GridDim = 256;
 const int K = 1024;// Number of Topics.
 const int numStreams = 2;
-const double beta = 0.1;
+const double beta = 0.01;
 const double alpha = 50.0 / K;
 
 #endif
