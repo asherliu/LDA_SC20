36c0525c958d47ef9df4fde9e57a076db45f1de4
wojiushishen <wangshilong789@gmail.com>
2020-06-05 20:09:57 -0400

    Add files via upload

--- EZLDAPairStorage/mainKernel.cu
+++ EZLDAPairStorage/mainKernel.cu
@@ -49,12 +49,12 @@ int main(int argc, char *argv[]) {
 	int maxChunkWTLength;
 	int numOfWordD;
 	int numOfWordS;
-	int numChunks = 16;
-	int numIters = 300;
+	int numChunks = 8;
+	int numIters = 100;
 
 	int chunksPerStream = numChunks / numStreams;
 
-	string chunkFilePrefix = "/gpfs/alpine/proj-shared/csc289/lda/datasets/data200k";
+	string chunkFilePrefix = "/gpfs/alpine/proj-shared/csc289/lda/datasets/pubmed";
 
 	
 	ofstream SamplingDRecord((chunkFilePrefix + string("/SamplingDRecord.txt")).c_str(), ios::binary);
